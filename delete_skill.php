9e187122c912e19ade7af939e7c7d890cf17e64d
siddhikale31 <siddhihkale@gmail.com>
2026-03-28 20:57:05 +0530

    Update delete_skill.php

--- delete_skill.php
+++ delete_skill.php
@@ -1,24 +1,30 @@
 <?php
 session_start();
-require 'db.php';
+require_once 'db.php';
+
 if (!isset($_SESSION['user_id'])) {
     die("User not logged in");
 }
 
+$user_id = $_SESSION['user_id'];
 $skill_id = $_POST['skill_id'];
 
-if(empty($skill_id)){
+if (empty($skill_id)) {
     die("Skill ID required");
 }
 
 $sql = "DELETE FROM user_skills
-        WHERE skill_id = :skill_id";
+        WHERE skill_id = :skill_id
+        AND user_id = :user_id";
 
 $stmt = $pdo->prepare($sql);
 
 $stmt->execute([
-    ':skill_id' => $skill_id
+    ':skill_id' => $skill_id,
+    ':user_id' => $user_id
 ]);
 
-echo "Skill deleted successfully";
+header("Location: _dashboard.php");
+exit();
 ?>
+
